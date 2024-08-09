48e9ac4693ebd64a16f7310a620139cad8c1f96d
Shanks <cjinhuo@qq.com>
2024-08-09 16:29:53 +0800

    feat(online-demo): :sparkles: set dark mode before react render

--- app/online-demo/src/hooks/useThemeMode.ts
+++ app/online-demo/src/hooks/useThemeMode.ts
@@ -1,14 +1,16 @@
 import { useEffect, useState } from 'react'
-import { ThemeMode } from '../shared/constants'
+import { LOCAL_STORAGE_THEME_KEY, ThemeMode } from '../shared/constants'
 import { getThemeMode } from '../shared/utils'
 
 export default function useDarkMode() {
 	const [theme, setTheme] = useState<ThemeMode>(getThemeMode())
 	useEffect(() => {
 		if (theme === ThemeMode.dark) {
-			document.body.classList.add(ThemeMode.dark)
+			document.documentElement.classList.add(ThemeMode.dark)
+			localStorage.setItem(LOCAL_STORAGE_THEME_KEY, ThemeMode.dark)
 		} else {
-			document.body.classList.remove(ThemeMode.dark)
+			localStorage.removeItem(LOCAL_STORAGE_THEME_KEY)
+			document.documentElement.classList.remove(ThemeMode.dark)
 		}
 	}, [theme])
 	return [theme, setTheme] as const
