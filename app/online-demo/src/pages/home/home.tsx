cba1283aee72a9540bfc66f5270017dce302f024
Shanks <cjinhuo@qq.com>
2024-08-10 18:07:30 +0800

    chore(online-demo): :hammer: update demo importing

--- app/online-demo/src/pages/home/home.tsx
+++ app/online-demo/src/pages/home/home.tsx
@@ -6,11 +6,10 @@ import * as TextSearchEngine from 'text-search-engine'
 ;(window as any)['_TEXT_SEARCH_ENGINE_'] = TextSearchEngine
 
 export default function Home() {
-	console.log('source: mito 前端监控,target: mi jkon')
+	console.log('source: mito 前端监控,target: mi jkon, result:', TextSearchEngine.search('mito 前端监控', 'mi jkon'))
 	console.log(TextSearchEngine.highlightMatches('mito 前端监控', 'mi jkon '))
 	return (
 		<div>
-			{TextSearchEngine.search('nod你的名e', 'no')}
 			<Button variant='outlined'>Home</Button>
 		</div>
 	)
