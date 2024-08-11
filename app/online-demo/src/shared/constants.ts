01398aac02f1b5d410fa12170bd439002e6a0b2b
Shanks <cjinhuo@qq.com>
2024-08-11 12:50:02 +0800

    feat(online-demo): :sparkles: new names for iconpark

--- app/online-demo/src/shared/constants.ts
+++ app/online-demo/src/shared/constants.ts
@@ -5,7 +5,9 @@ export enum IconParkNames {
 	'keyboard-2' = 'keyboard-2',
 	dark = 'dark',
 	light = 'light',
-	'search-engine' = 'search-engine',
+	'text-search' = 'text-search',
+	add = 'add',
+	delete = 'delete',
 }
 
 export const LOCAL_STORAGE_THEME_KEY = 'theme'
