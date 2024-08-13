cb9762d31cdf1921349868b535682cc7a73b60e6
czh <2244356600@qq.com>
2024-08-13 14:50:25 +0800

    feat: New store.js library and new popup logic on homepage
    # Conflicts:
    #	app/online-demo/src/pages/home/index.tsx

--- app/online-demo/src/pages/home/index.tsx
+++ app/online-demo/src/pages/home/index.tsx
@@ -1,10 +1,41 @@
-import { Container, CssBaseline, Grid2 as Grid } from '@mui/material'
+import { Container, CssBaseline, Grid2 as Grid, Link } from '@mui/material'
+import type { SnackbarOrigin } from '@mui/material/Snackbar'
+import react, { useMemo, useState } from 'react'
 import * as TextSearchEngine from 'text-search-engine'
 import ListSearch from '../../components/ListSearch'
+import SnackTip from '../../components/SnackTip'
+import { GITHUB_URL } from '../../config'
+import { useNotifyStar } from '../../hooks/useNotifyStar'
 
 window._TEXT_SEARCH_ENGINE_ = TextSearchEngine
-
+interface State extends SnackbarOrigin {
+	open: boolean
+}
 export default function Home() {
+	const [notificState, setNotificState] = useState<State>({
+		open: false,
+		vertical: 'top',
+		horizontal: 'center',
+	})
+	const { open } = notificState
+	const duration = useMemo(() => 1000 * 6, [])
+	const openNotification = (newState: SnackbarOrigin) => {
+		setNotificState({ ...newState, open: true })
+	}
+	useNotifyStar({ callback: openNotification, open })
+	const tipsContent = useMemo(() => {
+		return (
+			<div>
+				<p>
+					Your support is our greatest power, if you think our program is not bad, help move your finger to point a
+					praise!
+				</p>
+				<p>
+					github url: <Link href={GITHUB_URL}>{GITHUB_URL}</Link>
+				</p>
+			</div>
+		)
+	}, [])
 	return (
 		<div>
 			<CssBaseline />
@@ -14,12 +45,17 @@ export default function Home() {
 						<Grid size={{ xs: 24, md: 12 }}>
 							<ListSearch />
 						</Grid>
-						{/* <Grid size={{ xs: 12, md: 6 }}>
-							<TextHighlight originalText={LONG_TEXT} />
-						</Grid> */}
 					</Grid>
 				</Container>
 			</div>
+			<SnackTip
+				content={tipsContent}
+				state={notificState}
+				handleClose={() => {
+					setNotificState({ ...notificState, open: false })
+				}}
+				duration={duration}
+			/>
 		</div>
 	)
 }
