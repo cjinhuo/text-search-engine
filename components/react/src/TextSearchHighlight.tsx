47474cd932e93517772a6c81d1e5a452d9efa6a1
mon1cakk <435421355@qq.com>
2024-09-09 22:23:41 +0800

    style: update HighlightComponent CSS

--- components/react/src/TextSearchHighlight.tsx
+++ components/react/src/TextSearchHighlight.tsx
@@ -1,4 +1,4 @@
-import type React from 'react'
+import React from 'react'
 import { useCallback, useEffect, useRef, useState } from 'react'
 import styled from 'styled-components'
 import { type Matrix, search } from 'text-search-engine'
