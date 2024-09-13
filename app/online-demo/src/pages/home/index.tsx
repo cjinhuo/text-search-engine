4ca29e931a0e9d9716fcfd102fa30e4db94330e9
Shanks <cjinhuo@qq.com>
2024-09-13 11:34:11 +0800

    chore(online-demo): :hammer: update demo

--- app/online-demo/src/pages/home/index.tsx
+++ app/online-demo/src/pages/home/index.tsx
@@ -1,6 +1,5 @@
 import { Container, CssBaseline, Grid2 as Grid } from '@mui/material'
 import * as TextSearchEngine from 'text-search-engine'
-import { HighlightWithTarget } from 'text-search-engine/react'
 import ListSearch from '../../components/ListSearch'
 import ShowTip from '../../components/showTip'
 
@@ -14,8 +13,7 @@ export default function Home() {
 				<Container sx={{ mt: 4, mb: 4, flex: 1, maxWidth: '80vw!important', width: '100%' }}>
 					<Grid container spacing={3}>
 						<Grid size={{ xs: 24, md: 12 }}>
-							{/* <ListSearch /> */}
-							<HighlightWithTarget source='Node.js 最强监控平台 V9' target='nodejk' />
+							<ListSearch />
 						</Grid>
 					</Grid>
 				</Container>
