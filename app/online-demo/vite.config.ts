aedb504d347f564e9deb05210c8f16a54d24a91a
Shanks <cjinhuo@qq.com>
2024-10-19 17:26:10 +0800

    chore(architecture): :hammer: optimize bundlIng

--- app/online-demo/vite.config.ts
+++ app/online-demo/vite.config.ts
@@ -8,4 +8,17 @@ export default defineConfig({
 	server: {
 		open: '/text-search-engine',
 	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					// 把相关包单独打包成一个文件
+					reactVendor: ['react', 'react-dom', 'react-router-dom'],
+					//
+					search: ['text-search-engine'],
+					ui: ['@mui/material', '@mui/styles', '@emotion/react', '@emotion/styled'],
+				},
+			},
+		},
+	},
 })
