ca59e30eefc0b6a5e4bdf876c052902430f593cd
Shanks <cjinhuo@qq.com>
2024-10-19 17:46:29 +0800

    chore(architecture): :hammer: add sw

--- app/online-demo/vite.config.ts
+++ app/online-demo/vite.config.ts
@@ -1,9 +1,39 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			strategies: 'generateSW',
+			workbox: {
+				// 其他 Workbox 配置选项
+			},
+			manifest: {
+				name: 'My App',
+				short_name: 'App',
+				start_url: '/',
+				display: 'standalone',
+				background_color: '#ffffff',
+				description: 'My awesome app',
+				icons: [
+					{
+						src: '/icon-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: '/icon-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+				],
+			},
+		}),
+	],
 	base: '/text-search-engine',
 	server: {
 		open: '/text-search-engine',
